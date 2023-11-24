942d9ae6b428773131b3f4b624c95c31c725bd9a
Ray Wesley <raywesleypereira@gmail.com>
2023-11-24 07:35:19 -0300

    Prova_P2: Album de fotos de gatinhos

---
+++ README.md
@@ -0,0 +1,3 @@
+fatec_prova_p2 
+
+Aluno: Ray Wesley Pereira do Nascimento - RA: 2040482123045
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:fatec_p2/app.dart/api_gatinhos.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
